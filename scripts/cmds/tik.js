448c9328978a0e4f733ab0da0353f42a32641bd4
NTKhang <nthanhkhang2003@gmail.com>
2022-09-19 09:41:13 +0700

    fix api

--- scripts/cmds/tik.js
+++ scripts/cmds/tik.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 const qs = require("qs");
+const cheerio = require("cheerio");
 const { getStreamFromURL } = global.utils;
 
 module.exports = {
 	config: {
 		name: "tik",
 		aliases: ["tiktok"],
-		version: "1.0",
+		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -23,17 +24,18 @@ module.exports = {
 			case "-v":
 			case "v": {
 				const data = await query(args[1]);
-				if (data.message)
-					return message.reply(data.message);
-				if (!data.links)
-					return message.reply("Vui lòng nhập url tiktok hợp lệ");
-				const msgSend = message.reply(`Đang tải video: {{${data.desc}}}...`);
-				const linksNoWatermark = data.links
-					.filter(x => x.t.indexOf("(NO watermark)") > -1)
-					.sort((a, b) => parseFloat(b.s.slice(0, b.s.indexOf(" "))) - parseFloat(a.s.slice(0, b.s.indexOf(" "))));
-				const streamFile = await getStreamFromURL(linksNoWatermark[0].a);
+				if (data.status == 'error') {
+					if (data.message == 'It seems that TikTok is changed something on their website, so we are not able to reach their data. Please wait for 5 minutes and try to request your link again. We are looking into this issue.')
+						return message.reply("Vui lòng nhập url tiktok hợp lệ");
+					else
+						return message.reply(data.message);
+				}
+
+				const msgSend = message.reply(`Đang tải video: {{${data.title}}}...`);
+				const linksNoWatermark = data.downloadUrl;
+				const streamFile = await getStreamFromURL(linksNoWatermark, 'video.mp4');
 				message.reply({
-					body: `Đã tải video {{${data.desc}}}\nCaption: {{${data.desc}}}`,
+					body: `Đã tải video {{${data.desc}}}`,
 					attachment: streamFile
 				}, async () => message.unsend((await msgSend).messageID));
 				break;
@@ -41,21 +43,16 @@ module.exports = {
 			case "audio":
 			case "a":
 			case "-a": {
-				const dataAudio = await query(args[1]);
-				if (dataAudio.message)
-					return message.reply(dataAudio.message);
-				let urlAudio, audioName = '';
-				if (dataAudio.music) {
-					urlAudio = dataAudio.music.playUrl;
-					audioName = dataAudio.music.title;
-				}
-				else {
-					if (!dataAudio.links)
+				const dataAudio = await query(args[1], true);
+				if (dataAudio.status == 'error') {
+					if (dataAudio.message == 'It seems that TikTok is changed something on their website, so we are not able to reach their data. Please wait for 5 minutes and try to request your link again. We are looking into this issue.')
 						return message.reply("Vui lòng nhập url tiktok hợp lệ");
-					urlAudio = dataAudio.links.find(x => x.t.indexOf("MP3 Audio") > -1).a;
+					else
+						return message.reply(dataAudio.message);
 				}
+				const urlAudio = dataAudio.downloadUrl, audioName = dataAudio.title;
 				const msgSendAudio = message.reply(`Đang tải audio {{"${audioName}"}}...`);
-				const streamFileAudio = await getStreamFromURL(urlAudio);
+				const streamFileAudio = await getStreamFromURL(urlAudio, "audio.mp3");
 				message.reply({
 					body: `Đã tải audio {{"${audioName}"}}`,
 					attachment: streamFileAudio
@@ -63,15 +60,37 @@ module.exports = {
 				break;
 			}
 			default: {
-				message.SynTaxError();
+				message.SyntaxError();
 			}
 		}
 	}
 };
 
-async function query(url) {
-	const result = (await axios.post("https://lovetik.com/api/ajax/search", qs.stringify({
-		query: url
-	}))).data;
-	return result;
+async function query(url, isMp3 = false) {
+	const { data: result } = await axios({
+		url: "https://ssstik.io/abc?url=dl",
+		method: "POST",
+		data: qs.stringify({
+			id: url
+		}),
+		"headers": {
+			"uset-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33"
+		}
+	});
+
+	const $ = cheerio.load(result);
+	if (result.includes('<div class="is-icon b-box warning">'))
+		throw {
+			status: "error",
+			message: $('p').text()
+		};
+
+	const allUrls = $('.result_overlay_buttons > a');
+
+	const format = {
+		status: 'success',
+		title: $('.maintext').text(),
+		downloadUrl: $(allUrls[isMp3 ? allUrls.length - 1 : 0]).attr('href')
+	};
+	return format;
 }
