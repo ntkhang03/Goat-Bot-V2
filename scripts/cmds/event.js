1334ece8208a97fdcd571cfccd0983913df5c5a3
NTKhang <nthanhkhang2003@gmail.com>
2022-10-01 18:19:29 +0700

    add function `install`
    Download and install a script from a url, url is the path to the script (raw)

--- scripts/cmds/event.js
+++ scripts/cmds/event.js
@@ -1,10 +1,11 @@
 const fs = require("fs-extra");
 const path = require("path");
+const axios = require("axios");
 
 module.exports = {
 	config: {
 		name: "event",
-		version: "1.0",
+		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
 		role: 2,
@@ -13,9 +14,23 @@ module.exports = {
 		category: "owner",
 		guide: "{pn} {{load}} <tên file lệnh>"
 			+ "\n{pn} {{loadAll}}"
+			+ "\n{pn} {{install}} <url> <tên file lệnh>: Tải về và load command event, url là đường dẫn tới file lệnh (raw)"
+
 	},
 
-	onStart: ({ args, message, api, threadModel, userModel, threadsData, usersData }) => {
+	onReaction: async function ({ Reaction, message, event, api, threadModel, userModel, threadsData, usersData }) {
+		const { author, messageID, data: { fileName, rawCode } } = Reaction;
+		if (event.userID != author)
+			return;
+		const { configCommands } = global.GoatBot;
+		const { log, loadScripts } = global.utils;
+		const infoLoad = loadScripts("cmds", fileName, log, configCommands, api, threadModel, userModel, threadsData, usersData, rawCode);
+		infoLoad.status == "success" ?
+			message.reply(`✅ | Đã load command event {{${infoLoad.name}}} thành công`, () => message.unsend(messageID))
+			: message.reply(`❌ | Load command event {{${infoLoad.name}}} thất bại với lỗi\n{{${infoLoad.error.stack}}}`, () => message.unsend(messageID));
+	},
+
+	onStart: async ({ args, message, api, threadModel, userModel, threadsData, usersData, commandName, event }) => {
 		const { configCommands } = global.GoatBot;
 		const { log, loadScripts } = global.utils;
 
@@ -45,6 +60,27 @@ module.exports = {
 				arraySucces.length > 0 ? `✅ | Đã load thành công ${arraySucces.length} command event` : ""
 					+ arrayFail.length > 0 ? `\n\n❌ | Load thất bại ${arrayFail.length} command\n${"❗" + arrayFail.join("\n❗ ")})` : ""
 			);
+		}
+		else if (args[0] == "install") {
+			const [url, fileName] = args.slice(1);
+			if (!url || !url.match(/(https?:\/\/(?:www\.|(?!www)))/))
+				return message.reply("⚠️ | Vui lòng nhập vào đường dẫn tới file lệnh");
+			if (!fileName || !fileName.endsWith(".js"))
+				return message.reply("⚠️ | Vui lòng nhập vào tên file lệnh");
+			const { data: rawCode } = await axios.get(url);
+			if (!rawCode)
+				return message.reply("⚠️ | Không thể lấy được mã lệnh");
+			if (fs.existsSync(path.join(__dirname, '..', 'events', fileName)))
+				return message.reply("⚠️ | File lệnh đã tồn tại, bạn có chắc chắn muốn ghi đè lên file lệnh cũ không?\nThả cảm xúc bất kì vào tin nhắn này để tiếp tục", (err, info) => {
+					global.GoatBot.onReaction.set(info.messageID, {
+						commandName,
+						author: event.senderID,
+						data: {
+							fileName,
+							rawCode
+						}
+					});
+				});
 		}
 		else
 			message.SyntaxError();
