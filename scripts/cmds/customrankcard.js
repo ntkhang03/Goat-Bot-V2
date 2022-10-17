f7c28a650bd8f92dd89521d4431443c00b09bfa6
NTKhang <nthanhkhang2003@gmail.com>
2022-10-17 09:57:00 +0700

    Update customrankcard.js

--- scripts/cmds/customrankcard.js
+++ scripts/cmds/customrankcard.js
@@ -1,9 +1,12 @@
 const axios = require("axios");
+const rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
+const checkUrlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+const hexColorRegex = /^#([0-9a-f]{6})$/i;
 
 module.exports = {
 	config: {
 		name: "customrankcard",
-		version: "1.0",
+		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -11,13 +14,14 @@ module.exports = {
 		longDescription: "Thiết kế thẻ rank theo ý bạn",
 		category: "rank",
 		guide: {
-			body: "{pn} {{[maincolor | subcolor | expcolor | expnextlevelcolor | alphasubcolor | textcolor | reset]}} <value>\nTrong đó: "
-				+ "\n  + {{Maincolor}}: background chính của thẻ rank {{(hex color or rgba or url image)}}"
-				+ "\n  + {{Subcolor}}: background phụ {{(hex color or rgba or url image)}}"
-				+ "\n  + {{Expcolor}}: màu của thanh exp hiện tại"
-				+ "\n  + {{Expnextlevelcolor}}: màu của thanh exp full"
-				+ "\n  + {{Alphasubcolor}}: độ mờ của background phụ (từ 0 -> 1)"
-				+ "\n  + {{Textcolor}}: màu của chữ {{(hex color or rgba)}}"
+			body: "{pn} {{[maincolor | subcolor | expcolor | expnextlevelcolor | alphasubcolor | textcolor | reset]}} <value>"
+				+ "\nTrong đó: "
+				+ "\n  + {{maincolor | background}}: background chính của thẻ rank {{(hex color or rgba or url image)}}"
+				+ "\n  + {{subcolor}}: background phụ {{(hex color or rgba or url image)}}"
+				+ "\n  + {{expcolor}}: màu của thanh exp hiện tại"
+				+ "\n  + {{expnextlevelcolor}}: màu của thanh exp full"
+				+ "\n  + {{alphasubcolor}}: độ mờ của background phụ (từ 0 -> 1)"
+				+ "\n  + {{textcolor}}: màu của chữ {{(hex color or rgba)}}"
 				+ "\n Reset: reset về mặc định"
 				+ "\n\nVí dụ:"
 				+ "\n   {pn} {{maincolor #fff000}}"
@@ -28,7 +32,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, threadsData, event, args }) {
-		if (!args[0] || !args[1])
+		if (!args[0])
 			return message.SyntaxError();
 
 		const { data } = await threadsData.get(event.threadID);
@@ -37,18 +41,20 @@ module.exports = {
 
 		const oldDesign = data.customRankCard;
 		const key = args[0].toLowerCase();
-		const value = args.slice(1).join(" ");
+		let value = args.slice(1).join(" ");
 
-		const rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
-		const checkUrlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
-		const hexColorRegex = /^#([0-9a-f]{6})$/i;
-
-		if (["subcolor", "maincolor", "expcolor", "textcolor", "expnextlevelcolor"].includes(key)) {
+		if (["subcolor", "maincolor", "background", "expcolor", "textcolor", "expnextlevelcolor"].includes(key)) {
 			if (value.match(checkUrlRegex)) {
 				// if image url
 				const contentType = (await axios.get(value)).headers["content-type"];
 				if (!contentType.includes("image"))
 					return message.reply("Url hình ảnh không hợp lệ, vui lòng chọn 1 url với trang đích là hình ảnh");
+			}
+			else if (event.messageReply && event.messageReply.attachments?.length > 0) {
+				// if image attachment
+				if (!["photo", "animated_image"].includes(event.messageReply.attachments[0].type))
+					return message.reply("File đính kèm không phải là hình ảnh");
+				value = event.messageReply.attachments[0].url;
 			}
 			else {
 				// if color
@@ -61,6 +67,7 @@ module.exports = {
 					oldDesign.sub_color = value;
 					break;
 				case "maincolor":
+				case "background":
 					oldDesign.main_color = value;
 					break;
 				case "expcolor":
@@ -83,7 +90,7 @@ module.exports = {
 				message.err(err);
 			}
 		}
-		else if (key == "alphasubcolor" || key == "alphasubcard") {
+		else if (["alphasubcolor", "alphasubcard"].includes(key)) {
 			if (parseFloat(value) < 0 && parseFloat(value) > 1)
 				return message.reply("Vui lòng chọn chỉ số trong khoảng từ 0 -> 1");
 			oldDesign.alpha_subcard = parseFloat(value);
