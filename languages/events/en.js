53fbea51f625712b16059be4a1dbf59321cd75e7
NTKhang <nthanhkhang2003@gmail.com>
2022-10-24 14:41:21 +0700

    you can customize the language according to this example

---
+++ languages/events/en.js
@@ -0,0 +1,40 @@
+module.exports = {
+	// You can customize the language here
+	autoUpdateThreadInfo: {},
+	checkwarn: {
+		text: {
+			warn: "Member %1 has been warned 3 times before and has been banned from the chat box\n- Name: %1\n- Uid: %2\n- To unban, please use the \"%3warn unban <uid>\" command (with uid is the uid of the person you want to unban)",
+			needPermission: "Bot needs administrator permission to kick banned members"
+		}
+	},
+	leave: {
+		text: {
+			session1: "morning",
+			session2: "noon",
+			session3: "afternoon",
+			session4: "evening",
+			leaveType1: "left the group",
+			leaveType2: "was kicked from the group"
+		}
+	},
+	logsbot: {
+		text: {
+			title: "====== Bot logs ======",
+			added: "\n✅\nEvent: bot has been added to a new group\n- Added by: %1",
+			kicked: "\n❌\nEvent: bot has been kicked\n- Kicked by: %1",
+			footer: "\n- User ID: %1\n- Group: %2\n- Group ID: %3\n- Time: %4"
+		}
+	},
+	onEvent: {},
+	welcome: {
+		text: {
+			session1: "morning",
+			session2: "noon",
+			session3: "afternoon",
+			session4: "evening",
+			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
+			multiple1: "you",
+			multiple2: "you guys"
+		}
+	}
+};
