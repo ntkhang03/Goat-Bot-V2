ac7877cdd59c67d10c52213d2416b50b18b92211
NTKhang <nthanhkhang2003@gmail.com>
2022-11-15 13:37:28 +0700

    the structure of the source web has changed, I fixed it

--- scripts/cmds/moon.js
+++ scripts/cmds/moon.js
@@ -9,6 +9,113 @@ const agent = new https.Agent({
 });
 const { getStreamFromURL } = global.utils;
 
+module.exports = {
+	config: {
+		name: "moon",
+		version: "1.3",
+		author: "NTKhang",
+		countDown: 5,
+		role: 0,
+		shortDescription: {
+			vi: "xem ảnh mặt trăng",
+			en: "view moon image"
+		},
+		longDescription: {
+			vi: "xem ảnh mặt trăng vào đêm bạn chọn (dd/mm/yyyy)",
+			en: "view moon image on the night you choose (dd/mm/yyyy)"
+		},
+		category: "image",
+		guide: {
+			vi: "  {pn} <ngày/tháng/năm>"
+				+ "\n   {pn} <ngày/tháng/năm> <caption>",
+			en: "  {pn} <day/month/year>"
+				+ "\n   {pn} <day/month/year> <caption>"
+		}
+	},
+
+	langs: {
+		vi: {
+			invalidDateFormat: "Vui lòng nhập ngày/tháng/năm hợp lệ theo định dạng DD/MM/YYYY",
+			error: "Đã xảy ra lỗi không thể lấy ảnh mặt trăng của ngày %1",
+			invalidDate: "Ngày %1 không hợp lệ",
+			caption: "- Ảnh mặt trăng vào đêm %1"
+		},
+		en: {
+			invalidDateFormat: "Please enter a valid date in DD/MM/YYYY format",
+			error: "An error occurred while getting the moon image of %1",
+			invalidDate: "%1 is not a valid date",
+			caption: "- Moon image on %1"
+		}
+	},
+
+	onStart: async function ({ args, message, getLang }) {
+		const date = checkDate(args[0]);
+		if (!date)
+			return message.reply(getLang("invalidDateFormat"));
+		const linkCrawl = `https://lunaf.com/lunar-calendar/${date}`;
+
+		let html;
+		try {
+			html = await axios.get(linkCrawl, { httpsAgent: agent });
+		}
+		catch (err) {
+			return message.reply(getLang("error", args[0]));
+		}
+
+		const $ = cheerio.load(html.data);
+		const href = $("figure img").attr("data-ezsrcset");
+		const number = href.match(/phase-(\d+)\.png/)[1];
+		const imgSrc = moonImages[Number(number)];
+		const { data: imgSrcBuffer } = await axios.get(imgSrc, {
+			responseType: "arraybuffer"
+		});
+
+		const msg = getLang("caption", args[0])
+			+ `\n- ${$($('h3').get()[0]).text()}`
+			+ `\n- ${$("#phimg > small").text()}`
+			+ `\n- ${linkCrawl}`
+			+ `\n- https://lunaf.com/img/moon/h-phase-${number}.png`;
+
+		if (args[1]) {
+			const canvas = Canvas.createCanvas(1080, 2400);
+			const ctx = canvas.getContext("2d");
+			ctx.fillStyle = "black";
+			ctx.fillRect(0, 0, 1080, 2400);
+
+			const moon = await Canvas.loadImage(imgSrcBuffer);
+			centerImage(ctx, moon, 1080 / 2, 2400 / 2, 970, 970);
+
+			ctx.font = "60px \"Kanit SemiBold\"";
+			const wrapText = getLines(ctx, args.slice(1).join(" "), 594);
+			ctx.textAlign = "center";
+			ctx.fillStyle = "white";
+
+			const yStartText = 2095;//2042;
+			//ctx.fillRect(0, 2042, 1080, 5);
+			let heightText = yStartText - wrapText.length / 2 * 75;
+			for (const text of wrapText) {
+				ctx.fillText(text, 750, heightText);
+				heightText += 75;
+			}
+
+			const pathSave = __dirname + "/tmp/wallMoon.png";
+			fs.writeFileSync(pathSave, canvas.toBuffer());
+			message.reply({
+				body: msg,
+				attachment: fs.createReadStream(pathSave)
+			}, () => fs.unlinkSync(pathSave));
+		}
+		else {
+			const streamImg = await getStreamFromURL(imgSrc);
+			message.reply({
+				body: msg,
+				attachment: streamImg
+			});
+		}
+	}
+};
+
+
 const pathFont = __dirname + "/assets/font/Kanit-SemiBoldItalic.ttf";
 Canvas.registerFont(pathFont, {
 	family: "Kanit SemiBold"
@@ -80,108 +187,3 @@ const moonImages = [
 	'https://i.ibb.co/Rgj9vpj/moon-30.png',
 	'https://i.ibb.co/s5z0w9R/moon-31.png'
 ];
-
-module.exports = {
-	config: {
-		name: "moon",
-		version: "1.2",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		shortDescription: {
-			vi: "xem ảnh mặt trăng",
-			en: "view moon image"
-		},
-		longDescription: {
-			vi: "xem ảnh mặt trăng vào đêm bạn chọn (dd/mm/yyyy)",
-			en: "view moon image on the night you choose (dd/mm/yyyy)"
-		},
-		category: "image",
-		guide: {
-			vi: "  {pn} <ngày/tháng/năm>"
-				+ "\n   {pn} <ngày/tháng/năm> <caption>",
-			en: "  {pn} <day/month/year>"
-				+ "\n   {pn} <day/month/year> <caption>"
-		}
-	},
-
-	langs: {
-		vi: {
-			invalidDateFormat: "Vui lòng nhập ngày/tháng/năm hợp lệ theo định dạng DD/MM/YYYY",
-			error: "Đã xảy ra lỗi không thể lấy ảnh mặt trăng của ngày %1",
-			invalidDate: "Ngày %1 không hợp lệ",
-			caption: "- Ảnh mặt trăng vào đêm %1"
-		},
-		en: {
-			invalidDateFormat: "Please enter a valid date in DD/MM/YYYY format",
-			error: "An error occurred while getting the moon image of %1",
-			invalidDate: "%1 is not a valid date",
-			caption: "- Moon image on %1"
-		}
-	},
-
-	onStart: async function ({ args, message, getLang }) {
-		const date = checkDate(args[0]);
-		if (!date)
-			return message.reply(getLang("invalidDateFormat"));
-		const linkCrawl = `https://lunaf.com/lunar-calendar/${date}`;
-
-		let html;
-		try {
-			html = await axios.get(linkCrawl, { httpsAgent: agent });
-		}
-		catch (err) {
-			return message.reply(getLang("error", args[0]));
-		}
-
-		const $ = cheerio.load(html.data);
-		const href = $("figure.mimg img").attr("src");
-		const imgSrc = moonImages[Number(href.match(/\d+/)[0])];
-		const { data: imgSrcBuffer } = await axios.get(imgSrc, {
-			responseType: "arraybuffer"
-		});
-
-		const msg = getLang("caption", args[0])
-			+ `\n- ${$($('h3').get()[0]).text()}`
-			+ `\n- ${$("#phimg > small").text()}`
-			+ `\n- ${linkCrawl}`
-			+ `\n- https://lunaf.com/img/moon/h-${href.slice(href.lastIndexOf("/") + 1)}`;
-
-		if (args[1]) {
-			const canvas = Canvas.createCanvas(1080, 2400);
-			const ctx = canvas.getContext("2d");
-			ctx.fillStyle = "black";
-			ctx.fillRect(0, 0, 1080, 2400);
-
-			const moon = await Canvas.loadImage(imgSrcBuffer);
-			centerImage(ctx, moon, 1080 / 2, 2400 / 2, 970, 970);
-
-			ctx.font = "60px \"Kanit SemiBold\"";
-			const wrapText = getLines(ctx, args.slice(1).join(" "), 594);
-			ctx.textAlign = "center";
-			ctx.fillStyle = "white";
-
-			const yStartText = 2095;//2042;
-			//ctx.fillRect(0, 2042, 1080, 5);
-			let heightText = yStartText - wrapText.length / 2 * 75;
-			for (const text of wrapText) {
-				ctx.fillText(text, 750, heightText);
-				heightText += 75;
-			}
-
-			const pathSave = __dirname + "/tmp/wallMoon.png";
-			fs.writeFileSync(pathSave, canvas.toBuffer());
-			message.reply({
-				body: msg,
-				attachment: fs.createReadStream(pathSave)
-			}, () => fs.unlinkSync(pathSave));
-		}
-		else {
-			const streamImg = await getStreamFromURL(imgSrc);
-			message.reply({
-				body: msg,
-				attachment: streamImg
-			});
-		}
-	}
-};
