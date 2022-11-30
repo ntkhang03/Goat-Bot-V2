39ef8ff83cac27613e10f74ae766bd1c1bac6a8e
NTKhang <nthanhkhang2003@gmail.com>
2022-11-30 17:21:58 +0700

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -32,17 +32,17 @@ module.exports = {
 	},
 
 	onLoad: function ({ api }) {
-		const pathFile = __dirname + "/tmp/restart.txt";
+		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage("✅ | Bot restarted\n" + `⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
 
 	onStart: async function ({ message, event, getLang }) {
-		const pathFile = __dirname + "/tmp/restart.txt";
-		fs.writeFileSync(pathFile, event.threadID + ' ' + Date.now());
+		const pathFile = `${__dirname}/tmp/restart.txt`;
+		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
 		await message.reply(getLang("restartting"));
 		process.exit(2);
 	}
