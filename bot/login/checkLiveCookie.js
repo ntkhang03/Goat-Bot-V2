a0b62f164eb0f40a9b65b5186e12b5a129d75d91
NTKhang <nthanhkhang2003@gmail.com>
2022-12-30 16:12:03 +0700

    code optimization

--- bot/login/checkLiveCookie.js
+++ bot/login/checkLiveCookie.js
@@ -1,13 +1,18 @@
 const axios = require("axios");
 
 module.exports = async function (cookie, userAgent) {
-	const response = await axios({
-		url: 'https://mbasic.facebook.com/settings',
-		method: "GET",
-		headers: {
-			cookie,
-			'user-agent': userAgent || 'Mozilla/5.0 (Linux; Android 12; M2102J20SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Mobile Safari/537.36'
-		}
-	});
-	return response.data.includes('/privacy/xcs/action/logging/') || response.data.includes('/notifications.php?');
+	try {
+		const response = await axios({
+			url: 'https://mbasic.facebook.com/settings',
+			method: "GET",
+			headers: {
+				cookie,
+				'user-agent': userAgent || 'Mozilla/5.0 (Linux; Android 12; M2102J20SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Mobile Safari/537.36'
+			}
+		});
+		return response.data.includes('/privacy/xcs/action/logging/') || response.data.includes('/notifications.php?');
+	}
+	catch (e) {
+		return false;
+	}
 };
