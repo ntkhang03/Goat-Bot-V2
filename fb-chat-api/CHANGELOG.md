6f9d9bd34685cf1f67d64920ee7c92ae695f2075
NTKhang <nthanhkhang2003@gmail.com>
2023-02-18 18:27:29 +0700

    remove folder `fb-chat-api`

--- fb-chat-api/CHANGELOG.md
+++
@@ -1,2 +0,0 @@
-# Changelog
-Too lazy to write changelog, sorry! (will write changelog in the next release, through.)
--- fb-chat-api/src/getCurrentUserID.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = function(defaultFuncs, api, ctx) {
-  return function getCurrentUserID() {
-    return ctx.userID;
-  };
-};
