77c865ce1ef50a3fe518e7a36e71511e3f158a12
NTKhang <nthanhkhang2003@gmail.com>
2023-03-15 16:14:10 +0700

    added example how to use handlerWhenListenHasError

--- bot/login/handlerWhenListenHasError.js
+++ bot/login/handlerWhenListenHasError.js
@@ -1,3 +1,34 @@
-module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData }) {
+const axios = require('axios');
 
+// this is handler will run when listen has error (api.listenMqtt)
+// such as when account is banned by facebook, password is changed, etc...
+module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, error }) {
+	// YOUR CODE HERE
+
+	/* ___ Example send a mail to admin when bot has error ___ */
+	// const { utils } = global;
+	// const { sendMail, Prism } = utils;
+	// let highlightCode = error;
+	// if (typeof error == "object" && !error.stack)
+	// 	highlightCode = Prism.highlight(JSON.stringify(error, null, 2), Prism.languages.json, 'json');
+	// else if (error.stack)
+	// 	highlightCode = Prism.highlight(error.stack, Prism.languages.jsstacktrace, 'jsstacktrace');
+	// sendMail({
+	// 	to: "example@gmail.com", // <--- Change to your email
+	// 	subject: "Report error",
+	// 	text: "",
+	// 	html: `<h2>Has error when listen message in Goat Bot</h2><div><pre style="background:#272822;position: relative;padding: 1em 0 1em 1em;"><code style="color:#272822;background:#272822;text-shadow:0 1px rgba(0,0,0,.3);font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;">${highlightCode}</code></pre></div>`
+	// });
+
+	/* ___ Example send a message to telegram when bot has error ___ */
+	// const ADMIN_ID_TELEGRAM = "123456789"; // <--- Change to your telegram id
+	// let highlightCode = error;
+	// if (typeof error == "object" && !error.stack)
+	// 	highlightCode = JSON.stringify(error, null, 2);
+	// else if (error.stack)
+	// 	highlightCode = error.stack;
+	// await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
+	// 	chat_id: ADMIN_ID_TELEGRAM,
+	// 	text: `Has error when listen message in Goat Bot:\n${highlightCode}`
+	// });
 };
