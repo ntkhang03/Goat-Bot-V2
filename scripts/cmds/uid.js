24291636ad7f1fdbbdd565be0ef4d9bcaeb2bb8d
Thanh Khang <nthanhkhang2003@gmail.com>
2023-04-03 16:26:08 +0700

    Update the feature to view the uid of the response message

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -4,7 +4,7 @@ const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
 module.exports = {
 	config: {
 		name: "uid",
-		version: "1.1",
+		version: "1.2",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -20,10 +20,12 @@ module.exports = {
 		guide: {
 			vi: "   {pn}: dùng để xem id facebook của bạn"
 				+ "\n   {pn} @tag: xem id facebook của những người được tag"
-				+ "\n   {pn} <link profile>: xem id facebook của link profile",
+				+ "\n   {pn} <link profile>: xem id facebook của link profile"
+				+ "\n   Phản hồi tin nhắn của người khác kèm lệnh để xem id facebook của họ",
 			en: "   {pn}: use to view your facebook user id"
 				+ "\n   {pn} @tag: view facebook user id of tagged people"
 				+ "\n   {pn} <profile link>: view facebook user id of profile link"
+				+ "\n   Reply to someone's message with the command to view their facebook user id"
 		}
 	},
 
@@ -39,6 +41,8 @@ module.exports = {
 	onStart: async function ({ message, event, args, getLang }) {
 		if (!args[0])
 			return message.reply(event.senderID);
+		if (event.messageReply)
+			return message.reply(event.messageReply.senderID);
 		if (args[0].match(regExCheckURL)) {
 			let msg = '';
 			for (const link of args) {
@@ -53,6 +57,7 @@ module.exports = {
 			message.reply(msg);
 			return;
 		}
+
 		let msg = "";
 		const { mentions } = event;
 		for (const id in mentions)
