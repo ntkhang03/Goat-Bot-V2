3b9d684b0cfc65648e525b1e6135b98ab0b26517
Thanh Khang <nthanhkhang2003@gmail.com>
2023-04-07 16:47:48 +0700

    added the function to send notifications about email, telegram,... when mqtt server stops working due to account problems

--- bot/login/handlerWhenListenHasError.js
+++ bot/login/handlerWhenListenHasError.js
@@ -1,36 +1,87 @@
 const axios = require('axios');
-
+function filterAddress(address) {
+	return address.split(/[,;\s]/).map(id => id.trim()).filter(id => id);
+}
 // this is handler will run when listen has error (api.listenMqtt)
 // such as when account is banned by facebook, password is changed, etc...
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, error }) {
+	const { config } = global.GoatBot;
+	const { log } = global.utils;
+	const configNotiWhenListenMqttError = config.notiWhenListenMqttError || {};
 	// YOUR CODE HERE
 
 	/* ___ Example send a mail to admin when bot has error ___ */
-	// const { utils } = global;
-	// const { sendMail, Prism } = utils;
-	// let highlightCode = error;
-	// if (typeof error == "object" && !error.stack)
-	// 	highlightCode = Prism.highlight(JSON.stringify(error, null, 2), Prism.languages.json, 'json');
-	// else if (error.stack)
-	// 	highlightCode = Prism.highlight(error.stack, Prism.languages.jsstacktrace, 'jsstacktrace');
-	// sendMail({
-	// 	to: "example@gmail.com", // <--- Change to your email
-	// 	subject: "Report error",
-	// 	text: "",
-	// 	html: `<h2>Has error when listen message in Goat Bot</h2><div><pre style="background:#272822;position: relative;padding: 1em 0 1em 1em;"><code style="color:#272822;background:#272822;text-shadow:0 1px rgba(0,0,0,.3);font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;">${highlightCode}</code></pre></div>`
-	// });
+	if (configNotiWhenListenMqttError.gmail?.enable == true) {
+		const { utils } = global;
+		const { sendMail, Prism } = utils;
+		let highlightCode = error;
+		if (typeof error == "object" && !error.stack)
+			highlightCode = Prism.highlight(JSON.stringify(error, null, 2), Prism.languages.json, 'json');
+		else if (error.stack)
+			highlightCode = Prism.highlight(error.stack, Prism.languages.jsstacktrace, 'jsstacktrace');
+
+		const mailAddress = filterAddress(configNotiWhenListenMqttError.gmail.emailGetNoti);
+		for (const mail of mailAddress) {
+			if (!mail)
+				continue;
+			sendMail({
+				to: mail,
+				subject: "Report error",
+				text: "",
+				html: `<h2>Has error when listen message in Goat Bot</h2><div><pre style="background:#272822;position: relative;padding: 1em 0 1em 1em;"><code style="color:#272822;background:#272822;text-shadow:0 1px rgba(0,0,0,.3);font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;">${highlightCode}</code></pre></div>`
+			})
+				.then()
+				.catch(err => log.err("handlerWhenListenHasError", "Can not send mail to admin", err));
+		}
+	}
 
 	/* ___ Example send a message to telegram when bot has error ___ */
-	// const ADMIN_ID_TELEGRAM = "123456789"; // <--- Change to your telegram id
-	// const TELEBOT_TOKEN = "123456789:ABCDEF1234567890ABCDEF1234567890ABC"; // <--- Change to your telegram bot token	
-	// let highlightCode_ = error;
-	// if (typeof error == "object" && !error.stack)
-	// 	highlightCode_ = JSON.stringify(error, null, 2);
-	// else if (error.stack)
-	// 	highlightCode_ = error.stack;
-	// await axios.post(`https://api.telegram.org/bot${TELEBOT_TOKEN}/sendMessage`, {
-	// 	chat_id: ADMIN_ID_TELEGRAM,
-	// 	text: `Has error when listen message in Goat Bot:\n\`\`\`\n${highlightCode_}\n\`\`\``,
-	// 	parse_mode: "Markdown"
-	// });
+	if (configNotiWhenListenMqttError.telegram?.enable == true) {
+		const TELEBOT_TOKEN = configNotiWhenListenMqttError.telegram.botToken;
+		let highlightCode = error;
+		if (typeof error == "object" && !error.stack)
+			highlightCode = JSON.stringify(error, null, 2);
+		else if (error.stack)
+			highlightCode = error.stack;
+
+		const ADMIN_IDS_TELEGRAM = filterAddress(configNotiWhenListenMqttError.telegram.chatId);
+		for (const ADMIN_ID_TELEGRAM of ADMIN_IDS_TELEGRAM) {
+			if (!ADMIN_ID_TELEGRAM)
+				continue;
+			axios.post(`https://api.telegram.org/bot${TELEBOT_TOKEN}/sendMessage`, {
+				chat_id: ADMIN_ID_TELEGRAM,
+				text: `Has error when listen message in Goat Bot:\n\`\`\`\n${highlightCode}\n\`\`\``,
+				parse_mode: "Markdown"
+			})
+				.then()
+				.catch(err => log.err("handlerWhenListenHasError", "Can not send message to telegram", err.response?.data));
+		}
+	}
+
+	/* ___ Example send a message to webhook discord when bot has error ___ */
+	if (configNotiWhenListenMqttError.discordHook?.enable == true) {
+		let highlightCode = error;
+		if (typeof error == "object" && !error.stack)
+			highlightCode = JSON.stringify(error, null, 2);
+		else if (error.stack)
+			highlightCode = error.stack;
+
+		const jsonHook = {
+			content: `**Has error when listen message in Goat Bot:**\n\`\`\`\n${highlightCode}\n\`\`\``,
+			embeds: null,
+			attachments: []
+		};
+
+		const webhookUrls = filterAddress(configNotiWhenListenMqttError.discordHook.webhookUrl);
+		for (const WEBHOOK of webhookUrls) {
+			if (!WEBHOOK)
+				continue;
+			axios.post(WEBHOOK, jsonHook)
+				.then()
+				.catch(err => log.err("handlerWhenListenHasError", "Can not send message to discord webhook", err.response?.data));
+		}
+	}
+
+	/* AND YOU CAN CUSTOM YOUR CODE HERE */
+
 };
