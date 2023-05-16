8d20b27c95ba6716ece6c6b8c6a436fc2aa32d76
Thanh Khang <nthanhkhang2003@gmail.com>
2023-05-16 08:22:48 +0700

    add new subcommand: `cookies`, `string`

--- scripts/cmds/getfbstate.js
+++ scripts/cmds/getfbstate.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "getfbstate",
 		aliases: ["getstate"],
-		version: "1.1",
+		version: "1.0",
 		author: "NTKhang",
 		countDown: 5,
 		role: 2,
@@ -18,25 +18,48 @@ module.exports = {
 		},
 		category: "owner",
 		guide: {
-			en: "{pn}"
+			en: "   {pn}: get fbstate (appState)\n"
+				+ "   {pn} [cookies|cookie|c]: get fbstate with cookies format\n"
+				+ "   {pn} [string|str|s]: get fbstate with string format\n",
+			vi: "   {pn}: get fbstate (appState)\n"
+				+ "   {pn} [cookies|cookie|c]: get fbstate dạng cookies\n"
+				+ "   {pn} [string|str|s]: get fbstate dạng string\n"
 		}
 	},
 
 	langs: {
 		vi: {
-			success: "Đã gửi fbstate đến bạn, vui lòng check tin nhắn riêng của bot"
+			success: "Đã gửi fbstate đến bạn, vui lòng kiểm tra tin nhắn riêng của bot"
 		},
 		en: {
 			success: "Sent fbstate to you, please check bot's private message"
 		}
 	},
 
-	onStart: async function ({ message, api, event, getLang }) {
-		const fbstate = JSON.stringify(api.getAppState(), null, 2);
-		const pathSave = `${__dirname}/tmp/fbstate.json`;
+	onStart: async function ({ message, api, event, args, getLang }) {
+		let fbstate;
+		let extFile = "json";
+
+		if (["cookie", "cookies", "c"].includes(args[0])) {
+			fbstate = JSON.stringify(api.getAppState().map(e => ({
+				name: e.key,
+				value: e.value
+			})), null, 2);
+		}
+		else if (["string", "str", "s"].includes(args[0])) {
+			fbstate = api.getAppState().map(e => `${e.key}=${e.value}`).join("; ");
+			extFile = "txt";
+		}
+		else {
+			fbstate = JSON.stringify(api.getAppState(), null, 2);
+		}
+
+		const pathSave = `${__dirname}/tmp/fbstate.${extFile}`;
 		fs.writeFileSync(pathSave, fbstate);
+
 		if (event.senderID != event.threadID)
 			message.reply(getLang("success"));
+
 		api.sendMessage({
 			body: fbstate,
 			attachment: fs.createReadStream(pathSave)
