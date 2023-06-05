c2079c284ea3720471bad495585d7500c2d4a3b6
Thanh Khang <nthanhkhang2003@gmail.com>
2023-06-05 14:22:40 +0700

    add subcommand: `adminonly noti`: turn on/off the notification when user is not admin use bot

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -6,7 +6,7 @@ module.exports = {
 	config: {
 		name: "adminonly",
 		aliases: ["adonly", "onlyad", "onlyadmin"],
-		version: "1.2",
+		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
 		role: 2,
@@ -20,7 +20,10 @@ module.exports = {
 		},
 		category: "owner",
 		guide: {
-			en: "{pn} [on | off]"
+			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ admin mới có thể sử dụng bot"
+				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là admin sử dụng bot",
+			en: "   {pn} [on | off]: turn on/off the mode only admin can use bot"
+				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not admin use bot"
 		}
 	},
 
@@ -28,27 +31,43 @@ module.exports = {
 		vi: {
 			turnedOn: "Đã bật chế độ chỉ admin mới có thể sử dụng bot",
 			turnedOff: "Đã tắt chế độ chỉ admin mới có thể sử dụng bot",
-			syntaxError: "Sai cú pháp, chỉ có thể dùng {pn} on hoặc {pn} off"
+			turnedOnNoti: "Đã bật thông báo khi người dùng không phải là admin sử dụng bot",
+			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là admin sử dụng bot"
 		},
 		en: {
 			turnedOn: "Turned on the mode only admin can use bot",
 			turnedOff: "Turned off the mode only admin can use bot",
-			syntaxError: "Syntax error, only use {pn} on or {pn} off"
+			turnedOnNoti: "Turned on the notification when user is not admin use bot",
+			turnedOffNoti: "Turned off the notification when user is not admin use bot"
 		}
 	},
 
-	onStart: function ({ args, message, getLang  }) {
-		if (args[0] == "on") {
-			config.adminOnly.enable = true;
-			message.reply(getLang("turnedOn"));
-			fs.writeFileSync(client.dirConfig, JSON.stringify(config, null, 2));
+	onStart: function ({ args, message, getLang }) {
+		let isSetNoti = false;
+		let value;
+		let indexGetVal = 0;
+
+		if (args[0] == "noti") {
+			isSetNoti = true;
+			indexGetVal = 1;
 		}
-		else if (args[0] == "off") {
-			config.adminOnly.enable = false;
-			message.reply(getLang("turnedOff"));
-			fs.writeFileSync(client.dirConfig, JSON.stringify(config, null, 2));
-		}
+
+		if (args[indexGetVal] == "on")
+			value = true;
+		else if (args[indexGetVal] == "off")
+			value = false;
 		else
-			return message.reply(getLang("syntaxError"));
+			return message.SyntaxError();
+
+		if (isSetNoti) {
+			config.adminOnly.hideNotiMessage = !value;
+			message.reply(getLang(value ? "turnedOnNoti" : "turnedOffNoti"));
+		}
+		else {
+			config.adminOnly.enable = value;
+			message.reply(getLang(value ? "turnedOn" : "turnedOff"));
+		}
+
+		fs.writeFileSync(client.dirConfig, JSON.stringify(config, null, 2));
 	}
 };
