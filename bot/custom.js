68955529ebdb8679186c08b7a9c6e4a73e3e700e
Thanh Khang <nthanhkhang2003@gmail.com>
2023-06-25 16:40:24 +0700

    add new script bot/custom.js

---
+++ bot/custom.js
@@ -0,0 +1,16 @@
+const { log } = global.utils;
+
+module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
+	// This is where you can add your custom code to the bot.
+	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+
+	setInterval(async () => {
+		api.refreshFb_dtsg()
+			.then(() => {
+				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
+			})
+			.catch((err) => {
+				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
+			});
+	}, 1000 * 60 * 60 * 48); // 48h
+};
