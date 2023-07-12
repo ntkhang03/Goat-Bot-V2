612ff99ed67a2e0cd8f5a3649115c0f04364f37c
Thanh Khang <nthanhkhang2003@gmail.com>
2023-07-12 13:52:55 +0700

    automatically update changes of dirConfigCommands and dirConfig

--- Goat.js
+++ Goat.js
@@ -10,13 +10,44 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const google = require("googleapis").google;
 const nodemailer = require("nodemailer");
+const { execSync } = require('child_process');
+const log = require('./logger/log.js');
+const path = require("path");
 
 process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0; // Disable warning: "Warning: a promise was created in a handler but was not returned from it"
 
+function validJSON(pathDir) {
+	try {
+		if (!fs.existsSync(pathDir))
+			throw new Error(`File "${pathDir}" not found`);
+		execSync(`npx jsonlint ${pathDir}`, { stdio: 'pipe' });
+		return true;
+	}
+	catch (err) {
+		let msgError = err.message;
+		msgError = msgError.split("\n").slice(1).join("\n");
+		const indexPos = msgError.indexOf("    at");
+		msgError = msgError.slice(0, indexPos != -1 ? indexPos - 1 : msgError.length);
+		throw new Error(msgError);
+	}
+}
+
 const { NODE_ENV } = process.env;
-const dirConfig = `${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
-const dirConfigCommands = `${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
+// const dirConfig = `${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
+const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
+// const dirConfigCommands = `${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
+const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
 const dirAccount = `${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.dev.txt' : '.txt'}`;
+
+for (const pathDir of [dirConfig, dirConfigCommands]) {
+	try {
+		validJSON(pathDir);
+	}
+	catch (err) {
+		log.error("CONFIG", `Invalid JSON file "${pathDir.replace(__dirname, "")}":\n${err.message.split("\n").map(line => `  ${line}`).join("\n")}\nPlease fix it and restart bot`);
+		process.exit(0);
+	}
+}
 const config = require(dirConfig);
 if (config.whiteListMode?.whiteListIds && Array.isArray(config.whiteListMode.whiteListIds))
 	config.whiteListMode.whiteListIds = config.whiteListMode.whiteListIds.map(id => id.toString());
@@ -35,18 +66,18 @@ global.GoatBot = {
 	onReply: new Map(), // store all onReply
 	onReaction: new Map(), // store all onReaction
 	onAnyEvent: [], // store all onAnyEvent
-	config,
-	configCommands,
-	envCommands: {},
-	envEvents: {},
-	envGlobal: {},
+	config, // store config
+	configCommands, // store config commands
+	envCommands: {}, // store env commands
+	envEvents: {}, // store env events
+	envGlobal: {}, // store env global
 	reLoginBot: function () { }, // function relogin bot, will be set in bot/login/login.js
 	Listening: null, // store current listening handle
 	oldListening: [], // store old listening handle
 	callbackListenTime: {}, // store callback listen 
 	storage5Message: [], // store 5 message to check listening loop
 	fcaApi: null, // store fca api
-	botID: null
+	botID: null // store bot id
 };
 
 global.db = {
@@ -106,6 +137,44 @@ global.temp = {
 		events: {}
 	}
 };
+
+// watch dirConfigCommands file and dirConfig
+const watchAndReloadConfig = (dir, type, prop, logName) => {
+	let lastModified = fs.statSync(dir).mtimeMs;
+	let isFirstModified = true;
+
+	fs.watch(dir, (eventType) => {
+		if (eventType === type) {
+			const oldConfig = global.GoatBot[prop];
+
+			setTimeout(() => {
+				try {
+					if (isFirstModified) {
+						isFirstModified = false;
+						return;
+					}
+					if (lastModified === fs.statSync(dir).mtimeMs) {
+						return;
+					}
+					global.GoatBot[prop] = JSON.parse(fs.readFileSync(dir, 'utf-8'));
+					log.success(logName, `Reloaded ${dir}`);
+				} catch (err) {
+					log.warn(logName, `Can't reload ${dir}`);
+					global.GoatBot[prop] = oldConfig;
+				} finally {
+					lastModified = fs.statSync(dir).mtimeMs;
+				}
+			}, 200);
+		}
+	});
+};
+
+watchAndReloadConfig(dirConfigCommands, 'change', 'configCommands', 'CONFIG COMMANDS');
+watchAndReloadConfig(dirConfig, 'change', 'config', 'CONFIG');
+
+global.GoatBot.envGlobal = global.GoatBot.configCommands.envGlobal;
+global.GoatBot.envCommands = global.GoatBot.configCommands.envCommands;
+global.GoatBot.envEvents = global.GoatBot.configCommands.envEvents;
 
 // ———————————————— LOAD LANGUAGE ———————————————— //
 let pathLanguageFile = `${__dirname}/languages/${global.GoatBot.config.language}.lang`;
