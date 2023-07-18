6b84ca4e2cff4da69665c594fa5ba2f575c16cb0
Thanh Khang <nthanhkhang2003@gmail.com>
2023-07-18 13:15:08 +0700

    Copyright update 😮‍💨

--- Goat.js
+++ Goat.js
@@ -1,6 +1,20 @@
-/** 
+/**
  * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using 
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
+ *
+ * English:
+ * ! Please do not change the below code, it is very important for the project.
+ * It is my motivation to maintain and develop the project for free.
+ * ! If you change it, you will be banned forever
+ * Thank you for using
+ *
+ * Vietnamese:
+ * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
+ * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
+ * Cảm ơn bạn đã sử dụng
  */
 
 process.on('unhandledRejection', error => console.log(error));
@@ -33,9 +47,7 @@ function validJSON(pathDir) {
 }
 
 const { NODE_ENV } = process.env;
-// const dirConfig = `${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
 const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
-// const dirConfigCommands = `${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`;
 const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
 const dirAccount = `${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.dev.txt' : '.txt'}`;
 
@@ -325,9 +337,9 @@ function compareVersion(version1, version2) {
 	const v2 = version2.split(".");
 	for (let i = 0; i < 3; i++) {
 		if (parseInt(v1[i]) > parseInt(v2[i]))
-			return 1;
+			return 1; // version1 > version2
 		if (parseInt(v1[i]) < parseInt(v2[i]))
-			return -1;
+			return -1; // version1 < version2
 	}
-	return 0;
+	return 0; // version1 = version2
 }
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-/** 
+/**
  * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using 
- * 
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
+ *
  * English:
  * ! Please do not change the below code, it is very important for the project.
  * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned
+ * ! If you change it, you will be banned forever
  * Thank you for using
- * 
+ *
  * Vietnamese:
  * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
  * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
  * Cảm ơn bạn đã sử dụng
  */
 
