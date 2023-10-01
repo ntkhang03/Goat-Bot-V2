3063bad23f9aba7016da06274814b381309a4d18
NTKhang <nthanhkhang2003@gmail.com>
2023-10-01 12:48:53 +0700

    Update handlerCheckData.js

--- bot/handler/handlerCheckData.js
+++ bot/handler/handlerCheckData.js
@@ -1,7 +1,6 @@
 const { db, utils, GoatBot } = global;
 const { config } = GoatBot;
 const { log, getText } = utils;
-
 const { creatingThreadData, creatingUserData } = global.client.database;
 
 module.exports = async function (usersData, threadsData, event) {
@@ -9,13 +8,16 @@ module.exports = async function (usersData, threadsData, event) {
 	const senderID = event.senderID || event.author || event.userID;
 
 	// ———————————— CHECK THREAD DATA ———————————— //
-	if (threadID && isGroup && !db.allThreadData.some(t => t.threadID == threadID)) {
+	if (threadID && isGroup) {
 		try {
 			if (global.temp.createThreadDataError.includes(threadID))
 				return;
 
 			const findInCreatingThreadData = creatingThreadData.find(t => t.threadID == threadID);
 			if (!findInCreatingThreadData) {
+				if (global.db.allThreadData.some(t => t.threadID == threadID))
+					return;
+
 				const threadData = await threadsData.create(threadID);
 				log.info("DATABASE", `New Thread: ${threadID} | ${threadData.threadName} | ${config.database.type}`);
 			}
@@ -24,17 +26,22 @@ module.exports = async function (usersData, threadsData, event) {
 			}
 		}
 		catch (err) {
-			global.temp.createThreadDataError.push(threadID);
-			log.err("DATABASE", getText("handlerCheckData", "cantCreateThread", threadID), err);
+			if (err.name != "DATA_ALREADY_EXISTS") {
+				global.temp.createThreadDataError.push(threadID);
+				log.err("DATABASE", getText("handlerCheckData", "cantCreateThread", threadID), err);
+			}
 		}
 	}
 
 
 	// ————————————— CHECK USER DATA ————————————— //
-	if (senderID && !db.allUserData.some(u => u.userID == senderID)) {
+	if (senderID) {
 		try {
 			const findInCreatingUserData = creatingUserData.find(u => u.userID == senderID);
 			if (!findInCreatingUserData) {
+				if (db.allUserData.some(u => u.userID == senderID))
+					return;
+
 				const userData = await usersData.create(senderID);
 				log.info("DATABASE", `New User: ${senderID} | ${userData.name} | ${config.database.type}`);
 			}
@@ -43,7 +50,8 @@ module.exports = async function (usersData, threadsData, event) {
 			}
 		}
 		catch (err) {
-			log.err("DATABASE", getText("handlerCheckData", "cantCreateUser", senderID), err);
+			if (err.name != "DATA_ALREADY_EXISTS")
+				log.err("DATABASE", getText("handlerCheckData", "cantCreateUser", senderID), err);
 		}
 	}
 };
