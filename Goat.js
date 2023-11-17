88f693b524cc08523f104fb87dfb30d8154bbd47
NTKhang <nthanhkhang2003@gmail.com>
2023-11-17 20:54:13 +0700

    Update Goat.js

--- Goat.js
+++ Goat.js
@@ -191,59 +191,7 @@ global.GoatBot.envCommands = global.GoatBot.configCommands.envCommands;
 global.GoatBot.envEvents = global.GoatBot.configCommands.envEvents;
 
 // ———————————————— LOAD LANGUAGE ———————————————— //
-let pathLanguageFile = `${__dirname}/languages/${global.GoatBot.config.language}.lang`;
-if (!fs.existsSync(pathLanguageFile)) {
-	utils.log.warn("LANGUAGE", `Can't find language file ${global.GoatBot.config.language}.lang, using default language file "${__dirname}/languages/en.lang"`);
-	pathLanguageFile = `${__dirname}/languages/en.lang`;
-}
-const readLanguage = fs.readFileSync(pathLanguageFile, "utf-8");
-const languageData = readLanguage
-	.split(/\r?\n|\r/)
-	.filter(line => line && !line.trim().startsWith("#") && !line.trim().startsWith("//") && line != "");
-
-global.language = convertLangObj(languageData);
-function convertLangObj(languageData) {
-	const obj = {};
-	for (const sentence of languageData) {
-		const getSeparator = sentence.indexOf('=');
-		const itemKey = sentence.slice(0, getSeparator).trim();
-		const itemValue = sentence.slice(getSeparator + 1, sentence.length).trim();
-		const head = itemKey.slice(0, itemKey.indexOf('.'));
-		const key = itemKey.replace(head + '.', '');
-		const value = itemValue.replace(/\\n/gi, '\n');
-		if (!obj[head])
-			obj[head] = {};
-		obj[head][key] = value;
-	}
-	return obj;
-}
-
-function getText(head, key, ...args) {
-	let langObj;
-	if (typeof head == "object") {
-		let pathLanguageFile = path.normalize(`${__dirname}/languages/${head.lang}.lang`);
-		head = head.head;
-		if (!fs.existsSync(pathLanguageFile)) {
-			utils.log.warn("LANGUAGE", `Can't find language file ${pathLanguageFile}, using default language file "${path.normalize(`${__dirname}/languages/en.lang`)}"`);
-			pathLanguageFile = `${__dirname}/languages/en.lang`;
-		}
-		const readLanguage = fs.readFileSync(pathLanguageFile, "utf-8");
-		const languageData = readLanguage
-			.split(/\r?\n|\r/)
-			.filter(line => line && !line.trim().startsWith("#") && !line.trim().startsWith("//") && line != "");
-		langObj = convertLangObj(languageData);
-	}
-	else {
-		langObj = global.language;
-	}
-	if (!langObj[head]?.hasOwnProperty(key))
-		return `Can't find text: "${head}.${key}"`;
-	let text = langObj[head][key];
-	for (let i = args.length - 1; i >= 0; i--)
-		text = text.replace(new RegExp(`%${i + 1}`, 'g'), args[i]);
-	return text;
-}
-global.utils.getText = getText;
+const getText = global.utils.getText;
 
 // ———————————————— AUTO RESTART ———————————————— //
 if (config.autoRestart) {
