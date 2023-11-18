2c7e31edfd9d9f023f96a1a56dbbf0c76405a0cd
Mohammad Alamin <anbuinfosec@gmail.com>
2023-11-18 12:57:13 +0600

    Update videofb.js

--- scripts/cmds/videofb.js
+++ scripts/cmds/videofb.js
@@ -1,63 +1,67 @@
 const axios = require("axios");
-
 module.exports = {
 	config: {
 		name: "videofb",
-		version: "1.3",
-		author: "NTKhang & Mohammad Alamin",
+		aliases: ["fbdown", "fb"],
+		version: "1.1",
+		author: "Mohammad Alamin",
 		countDown: 5,
 		role: 0,
 		shortDescription: {
 			vi: "Tải video từ facebook",
-			en: "Download video from facebook"
+			en: "Download video from Facebook"
 		},
 		longDescription: {
 			vi: "Tải video/story từ facebook (công khai)",
-			en: "Download video/story from facebook (public)"
+			en: "Download video/story from Facebook (public)"
 		},
 		category: "media",
 		guide: {
 			vi: "   {pn} <url video/story>: tải video từ facebook",
-			en: "   {pn} <url video/story>: download video from facebook"
+			en: "   {pn} <url video/story>: download video from Facebook"
 		}
 	},
 
 	langs: {
 		vi: {
-			missingUrl: "Vui lòng nhập url video/story facebook (công khai) bạn muốn tải về",
+			missingUrl: "Vui lòng nhập url video/story Facebook (công khai) bạn muốn tải về",
 			error: "Đã xảy ra lỗi khi tải video",
 			downloading: "Đang tiến hành tải video cho bạn",
 			tooLarge: "Rất tiếc không thể tải video cho bạn vì dung lượng lớn hơn 83MB"
 		},
 		en: {
-			missingUrl: "Please enter the facebook video/story (public) url you want to download",
+			missingUrl: "Please enter the Facebook video/story (public) url you want to download",
+			missingKey: "Please add your global key on configCommands.json",
 			error: "An error occurred while downloading the video",
 			downloading: "Downloading video for you",
-			tooLarge: "Sorry, we can't download the video for you because the size is larger than 83MB"
+			tooLarge: "Sorry, we can't download the video for you."
 		}
 	},
 
-	onStart: async function ({ args, message, getLang }) {
-		if (!args[0]) {
-			return message.reply(getLang("missingUrl"));
-		}
-
+	onStart: async function ({ args, message, getLang, envGlobal }) {
+	  if (!args[0]) return message.reply(getLang("missingUrl"));
+		if (!envGlobal.globalKey) return message.reply(getLang("missingKey"));
 		let msgSend = null;
 		try {
-			const response = await axios.get(`https://toxinum.xyz/api/v1/videofb?url=${args[0]}`);
-
+		  msgSend = message.reply(getLang("downloading"));
+		  const response = await axios.get('https://anbusec.xyz/api/downloader/facebook',
+		  {
+		    params: {
+		      'apikey': envGlobal.globalKey,
+		      'url': args[0]
+		    }
+		  });
 			if (response.data.success === false) {
 				return message.reply(getLang("error"));
 			}
-
-			msgSend = message.reply(getLang("downloading"));
-
-			const stream = await global.utils.getStreamFromURL(response.data.url2); //url2 is for high quality videos & url1 is for low quality videos
-			await message.reply({ attachment: stream });
-
+			const stream = await global.utils.getStreamFromURL(response.data.url);
+			await message.reply({
+			  body: response.data.title,
+			  attachment: stream
+			});
 			message.unsend((await msgSend).messageID);
-		}
-		catch (e) {
+		} catch (e) {
+		  console.log(e)
 			message.unsend((await msgSend).messageID);
 			return message.reply(getLang("tooLarge"));
 		}
