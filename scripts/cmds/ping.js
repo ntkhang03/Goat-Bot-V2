40b2efbadaf45f94a534f12e876bdfc242d570c7
kshitiz-008 <142625808+kshitiz-008@users.noreply.github.com>
2024-02-08 00:01:43 +0545

    Create ping.js

---
+++ scripts/cmds/ping.js
@@ -0,0 +1,25 @@
+module.exports = {
+  config: {
+    name: "ping",
+    aliases: ["ms"],
+    version: "1.0",
+    author: "Sandu",
+    role: 0,
+    shortDescription: {
+      en: "Displays the current ping of the bot's system."
+    },
+    longDescription: {
+      en: "Displays the current ping of the bot's system."
+    },
+    category: "𝗜𝗡𝗙𝗢",
+    guide: {
+      en: "Use {p}ping to check the current ping of the bot's system."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const timeStart = Date.now();
+    await api.sendMessage("Checking Bot's ping", event.threadID);
+    const ping = Date.now() - timeStart;
+    api.sendMessage(`The current ping is ${ping}ms.`, event.threadID);
+  }
+};
