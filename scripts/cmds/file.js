1d48fcc7b7c12d2f4927f796a2e24bcc3dd6768a
xenon-187 <159904686+xenon-187@users.noreply.github.com>
2024-02-13 18:22:48 +0545

    Create file.js

---
+++ scripts/cmds/file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "sendfile",
+    aliases: ["file"],
+    version: "1.0",
+    author: "Mahir Tahsan",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100042408666316",];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+    }
+
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
