dbdad11f2df42428007fadc6ac544c87be1a64a8
NTKhang <nthanhkhang2003@gmail.com>
2024-02-24 18:27:19 +0700

    language update

--- scripts/cmds/warn.js
+++ scripts/cmds/warn.js
@@ -3,7 +3,7 @@ const { getTime } = global.utils;
 module.exports = {
 	config: {
 		name: "warn",
-		version: "1.6",
+		version: "1.7",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -86,7 +86,7 @@ module.exports = {
 			invalidUid4: "⚠️ You need to tag or reply to the message of the person you want to warn",
 			warnSuccess: "⚠️ Warned member %1 times %2\n- Uid: %3\n- Reason: %4\n- Date Time: %5\nThis member has been warned 3 times and banned from the box, to unban use the command \"%6warn unban <uid>\" (with uid is the uid of the person you want to unban)",
 			noPermission5: "⚠️ Bot needs administrator permissions to kick banned members",
-			warnSuccess2: "⚠️ Warned member %1 times %2\n- Uid: %3\n- Reason: %4\n- Date Time: %5\nIf this person violates %6 more times, they will be banned from the box",
+			warnSuccess2: "⚠️ Warned member %1 %2 times\n- Uid: %3\n- Reason: %4\n- Date Time: %5\nIf this person violates %6 more times, they will be banned from the box",
 			hasBanned: "⚠️ The following members have been warned 3 times before and banned from the box:\n%1",
 			failedKick: "⚠️ An error occurred when kicking the following members:\n%1",
 			userNotInGroup: "⚠️ The user \"%1\" is currently not in your group"
