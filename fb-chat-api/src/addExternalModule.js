be502791531f727d73c10c1ff30f44995da31501
NTKhang <nthanhkhang2003@gmail.com>
2024-03-04 13:28:09 +0700

    add 2 more parameters: `utils` and `log` when calling the function in `moduleObj`

--- fb-chat-api/src/addExternalModule.js
+++ fb-chat-api/src/addExternalModule.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const utils = require("../utils");
+const log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
 	return function addExternalModule(moduleObj) {
 		if (utils.getType(moduleObj) == "Object") {
 			for (const apiName in moduleObj) {
 				if (utils.getType(moduleObj[apiName]) == "Function") {
-					api[apiName] = moduleObj[apiName](defaultFuncs, api, ctx);
+					api[apiName] = moduleObj[apiName](defaultFuncs, api, ctx, utils, log);
 				} else {
 					throw new Error(`Item "${apiName}" in moduleObj must be a function, not ${utils.getType(moduleObj[apiName])}!`);
 				}
@@ -17,3 +18,12 @@ module.exports = function (defaultFuncs, api, ctx) {
 		}
 	};
 };
+
+// example usage:
+// api.addExternalModule({
+// 	getCtx: (defaultFuncs, api, ctx, utils, log) => {
+// 		return function getCtx() {
+// 			return ctx;
+// 		};
+// 	}
+// });
