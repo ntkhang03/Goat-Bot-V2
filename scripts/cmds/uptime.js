afe3bab7bfc563f1855df92a0cda97f016e85401
ManashJoshi <156304424+ManashJoshi@users.noreply.github.com>
2024-03-06 09:20:38 +0545

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -1,108 +1,33 @@
- const axios = require("axios");
- const fs = require("fs-extra");
- const ytdl = require("ytdl-core");
- const request = require("request");
- const yts = require("yt-search");
-
- const input = event.body;
- const text = input.substring(12);
- const data = input.split(" ");
 module.exports = {
-    config: {
-        name: "sing2",
-        version: "1.0",
-        author: "Itachi",
-        countDown: 5,
-        role: 0,
-        shortDescription: "singing command",
-        longDescription: "",
-        category: "fun",
-        guide: {
-			vi: "",
-			en: ""
+	config: {
+		name: "uptime",
+		aliases: ["up", "upt"],
+		version: "1.0",
+		author: "Itachi",
+		role: 0,
+		shortDescription: {
+			en: "Displays the uptime of the bot."
+		},
+		longDescription: {
+			en: "Displays the amount of time that the bot has been running for."
+		},
+		category: "System",
+		guide: {
+			en: "Use {p}uptime to display the uptime of the bot."
 		}
-    },
+	},
+	onStart: async function ({ api, event, args }) {
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} second`;
+		api.sendMessage(` Hello Master 😸
+ 
 
-    onStart: async function ({ message, event, args }) {
-        try {
-          let query = '';
+           Your Bot Has been running for
 
-    
-      if (event.type === "message_reply" && ["audio", "video"].includes(event.messageReply.attachments[0].type)) {
-        const attachmentUrl = event.messageReply.attachments[0].url;
-
-        
-        const transcription = await extractTextFromAudio(attachmentUrl);
-
-        
-        query = transcription.split('\n')[0];
-      } else {
-        
-        const input = event.body;
-        const data = input.split(" ");
-
-        if (data.length < 2) {
-          return message.reply("Please put a song" + "\n" + "Example: sing <song name>");
-        }
-        
-        data.shift();
-        query = data.join(" ");
-      }
-
-     
-      const originalMessage = await message.reply(`Searching for "${query}"...`);
-      const searchResults = await yts(query);
-
-      if (!searchResults.videos.length) {
-        return message.reply("Error: Song not found.");
-      } 
-            const video = searchResults.videos[0];
-      const videoUrl = video.url;
-      const stream = ytdl(videoUrl, { filter: "audioonly" });
-      const fileName = `music.mp3`;
-      const filePath = `${__dirname}/tmp/${fileName}`;
-
-      stream.pipe(fs.createWriteStream(filePath));
-
-      stream.on('response', () => {
-        console.info('[DOWNLOADER]', 'Starting download now!');
-      });
-
-      stream.on('info', (info) => {
-        console.info('[DOWNLOADER]',
- `Downloading ${info.videoDetails.title} by ${info.videoDetails.author.name}`);
-      });
-
-      stream.on('end', async () => {
-        console.info('[DOWNLOADER] Downloaded');
-        if (fs.statSync(filePath).size > 26214400) {
-          fs.unlinkSync(filePath);
-          return message.reply('[ERR] The file could not be sent because it is larger than 25MB.');
-        }
-        const replyMessage = {
-          body: `Title: ${video.title}\nArtist: ${video.author.name}`,
-          attachment: fs.createReadStream(filePath),
-        };
-        await api.unsendMessage(originalMessage.messageID);
-        await message.reply(replyMessage, event.threadID, () => {
-          fs.unlinkSync(filePath);
-        });
-      });
-    } catch (error) {
-      console.error('[ERROR]', error);
-      message.reply("An error occurred while processing the request.");
-    }
-  },
-};
-
-
-async function extractTextFromAudio(audioUrl) {
-  try {
-    const response = await axios.get(`https://milanbhandari.onrender.com/transcribe?url=${encodeURIComponent(audioUrl)}`);
-    return response.data.transcript || '';
-  } catch (error) {
-    console.error('[ERROR]', error);
-    return '';
-          
-      }
+${uptimeString}.`, event.threadID);
+	}
 };
