377b9be256c90a844419c717a54371ccc1604caf
Stanley stawa <159188314+Stanleystawa@users.noreply.github.com>
2024-03-24 17:50:04 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
+const doNotDelete = "[ 🤖 | Stanley stawa 😗🍷]";
 /**
 * @author NTKhang
 * @author: do not delete it
