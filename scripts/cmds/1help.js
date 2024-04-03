486b9d67de5837c2a636ea99bb9ae268b793e1a4
Merdi madimba🍀 <161202374+Zorolefrerot@users.noreply.github.com>
2024-04-03 22:56:36 +0100

    1help.js

--- scripts/cmds/1help.js
+++ scripts/cmds/1help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🔖 | 𝙈𝙚𝙧𝙙𝙞 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ 🐐 | GoatBot V2 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
   config: {
@@ -34,7 +34,9 @@ module.exports = {
       const categories = {};
       let msg = "";
 
-      msg += `🎯𝙇𝙐𝙉𝘼 𝙃𝙀𝙇𝙋 𝘾𝙈𝘿🎯 \n_____________________; // replace with your name 
+      msg += `──────⊹⊱✫⊰⊹──────
+\n   🎯𝙇𝙐𝙉𝘼̀ 𝘾𝙈𝘿🎯 \n──────⊹⊱✫⊰⊹──────
+`; // replace with your name 
 
       for (const [name, value] of commands) {
         if (value.config.role > 1 && role < value.config.role) continue;
@@ -46,26 +48,34 @@ module.exports = {
 
       Object.keys(categories).forEach((category) => {
         if (category !== "info") {
-          msg += `───────\n│『 ${category.toUpperCase()}  』` ;
+          msg += `\n╞═════𖠁
+\n│ 『
+  ${category.toUpperCase()}  』
+`;
 
 
           const names = categories[category].commands.sort();
           for (let i = 0; i < names.length; i += 3) {
-            const cmds = names.slice(i, i + 3).map((item) => `🎯${item}`);
+            const cmds = names.slice(i, i + 3).map((item) => `🔴${item}`);
             msg += `\n│ ${cmds.join(" ".repeat(Math.max(1, 10 - cmds.join("").length)))}`;
           }
 
-          msg += `\n══════════`;
+          msg += `\n|═════𖠁
+`;
         }
       });
 
       const totalCommands = commands.size;
-      msg += `\n 𝙇𝙚 𝙣𝙤𝙢𝙗𝙧𝙚 𝙩𝙤𝙩𝙖𝙡 𝙙𝙚𝙨 𝘾𝙈𝘿🎯 𝙚𝙨𝙩 𝙙𝙚 ${totalCommands} \n`;
-      msg += `𝙐𝙩𝙞𝙡𝙞𝙨𝙚 𝙡𝙚 𝙥𝙧𝙚𝙛𝙞𝙭 ${prefix} 𝙋𝙤𝙪𝙧 𝙥𝙤𝙪𝙫𝙤𝙞𝙧 𝙪𝙩𝙞𝙡𝙞𝙨𝙚𝙧 𝙩𝙤𝙪𝙩𝙚𝙨 𝙡𝙚𝙨 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙚𝙨 💦\n`;
-      msg += `💦 |𝙈𝙀𝙍𝘿𝙄'𝙎 𝘽𝙊𝙏`; // its not decoy so change it if you want 
+      msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
+      msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`;
+      msg += `🌹 | 𝙈𝙀𝙍𝘿𝙄'𝙎 𝘽𝙊𝙏`; // its not decoy so change it if you want 
 
       const helpListImages = [
-        "https://i.ibb.co/7Qh46VN/image.jpg", // add image link here,
+        "https://i.ibb.co/q9NHQH9/image.jpg", // add image link here
+        "https://i.ibb.co/q9NHQH9/image.jpg",
+        "https://i.ibb.co/mS73vLT/image.jpg",
+        "https://i.ibb.co/q9NHQH9/image.jpg",
+        "https://i.ibb.co/mS73vLT/image.jpg",
         // Add more image links as needed
       ];
 
@@ -92,20 +102,20 @@ module.exports = {
         const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
 
         const response = `___________⤴ 𝙉𝘼𝙈𝙀 __________⤴
-  ❍🎯 ${configCommand.name}
-  ❍🎯 𝙄𝙉𝙁𝙊
-  ❍🎯 𝘿𝙚𝙨𝙘𝙧𝙞𝙥𝙩𝙞𝙤𝙣: ${longDescription}
-  ❍🎯 𝙊𝙩𝙝𝙚𝙧 𝙣𝙖𝙢𝙚𝙨: ${configCommand.aliases ? configCommand.aliases.join(", ") : "Do not have"}
-  ❍🎯 𝙊𝙩𝙝𝙚𝙧 𝙣𝙖𝙢𝙚𝙨 𝙞𝙣 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥: 𝘿𝙤 𝙣𝙤𝙩 𝙝𝙖𝙫𝙚
-  ❍🎯 𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${configCommand.version || "1.0"}
-  ❍🎯 𝙍𝙤𝙡𝙚: ${roleText}
-  ❍🎯 𝙏𝙞𝙢𝙚 𝙥𝙚𝙧 𝙘𝙤𝙢𝙢𝙖𝙣𝙙: ${configCommand.countDown || 1}s
-  ❍🎯 𝘼𝙪𝙩𝙝𝙤𝙧: ${author}
-  ❍🎯 𝙐𝙨𝙖𝙜𝙚
-  ❍🎯 ${usage}
-  ❍🎯 𝙉𝙤𝙩𝙚𝙨
-  ❍🎯 𝙏𝙝𝙚 𝙘𝙤𝙣𝙩𝙚𝙣𝙩 𝙞𝙣𝙨𝙞𝙙𝙚 <𝙓𝙓𝙓𝙓𝙓> 𝙘𝙖𝙣 𝙗𝙚 𝙘𝙝𝙖𝙣𝙜𝙚𝙙
-  ❍🎯 𝙏𝙝𝙚 𝙘𝙤𝙣𝙩𝙚𝙣𝙩 𝙞𝙣𝙨𝙞𝙙𝙚 [𝙖|𝙗|𝙘] 𝙞𝙨 𝙖 𝙤𝙧 𝙗 𝙤𝙧 𝙘 \n_____________________\n 🔖 𝙚𝙙𝙞𝙩𝙚 𝙗𝙮 : 🔴𝙈𝙚𝙧𝙙𝙞🔵
+  ➡🎯 ${configCommand.name}
+  ➡🎯 𝙄𝙉𝙁𝙊
+  ➡🎯 𝘿𝙚𝙨𝙘𝙧𝙞𝙥𝙩𝙞𝙤𝙣: ${longDescription}
+  ➡🎯 𝙊𝙩𝙝𝙚𝙧 𝙣𝙖𝙢𝙚𝙨: ${configCommand.aliases ? configCommand.aliases.join(", ") : "Do not have"}
+  ➡🎯 𝙊𝙩𝙝𝙚𝙧 𝙣𝙖𝙢𝙚𝙨 𝙞𝙣 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥: 𝘿𝙤 𝙣𝙤𝙩 𝙝𝙖𝙫𝙚
+  ➡🎯 𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${configCommand.version || "1.0"}
+  ➡🎯 𝙍𝙤𝙡𝙚: ${roleText}
+  ➡🎯 𝙏𝙞𝙢𝙚 𝙥𝙚𝙧 𝙘𝙤𝙢𝙢𝙖𝙣𝙙: ${configCommand.countDown || 1}s
+  ➡🎯 𝘼𝙪𝙩𝙝𝙤𝙧: ${author}
+  ➡🎯 𝙐𝙨𝙖𝙜𝙚
+  ➡🎯 ${usage}
+  ➡🎯𝙉𝙤𝙩𝙚𝙨
+  ➡🎯 𝙏𝙝𝙚 𝙘𝙤𝙣𝙩𝙚𝙣𝙩 𝙞𝙣𝙨𝙞𝙙𝙚 <𝙓𝙓𝙓𝙓𝙓> 𝙘𝙖𝙣 𝙗𝙚 𝙘𝙝𝙖𝙣𝙜𝙚𝙙
+  ➡🎯 𝙏𝙝𝙚 𝙘𝙤𝙣𝙩𝙚𝙣𝙩 𝙞𝙣𝙨𝙞𝙙𝙚 [𝙖|𝙗|𝙘] 𝙞𝙨 𝙖 𝙤𝙧 𝙗 𝙤𝙧 𝙘 \n_____________________\n 🎯 𝙚𝙙𝙞𝙩𝙚 𝙗𝙮 : 🔴𝙈𝙀𝙍𝘿𝙄🔵
   `;
 
         await message.reply(response);
@@ -125,4 +135,4 @@ function roleTextToString(roleText) {
     default:
       return "Unknown role";
   }
-	}
+	  }
