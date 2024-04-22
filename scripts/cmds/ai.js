4fb25cdccebb119dd94a442b0c70ebff14fe06bc
Titus000 <158463994+Titus000@users.noreply.github.com>
2024-04-22 20:11:27 +0100

    Create ai.js

---
+++ scripts/cmds/ai.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+
+const Prefixes = [
+  '/ai', 
+  'titus',
+  'nemo',
+  'ai',
+  'ask',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("Bonjour, comment puis-je vous aidez ? 💖");
+        return;
+      }
+
+
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply(answer);
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
