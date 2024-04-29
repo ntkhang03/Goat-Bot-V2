4a10a0e42185e500a61b8c62a69d38fd56401156
resiakct <168367679+resiakct@users.noreply.github.com>
2024-04-29 08:43:27 +0800

    Update sim.js

--- scripts/cmds/sim.js
+++ scripts/cmds/sim.js
@@ -1,32 +1,69 @@
-export const setup = {
-  name: "sim",
-  version: "40.0.3",
-  permission: "Users",
-  creator: "John Lester",
-  description: "Talk with simsimi",
-  category: "chatbot",
-  usages: ["[ask]", "on", "off"],
-  cooldown: 10,
-  isPrefix: true
+const axios = require("axios");
+module.exports = {
+	config: {
+		name: 'sim',
+		version: '1.2',
+		author: 'KENLIEPLAYS',
+		countDown: 3,
+		role: 0,
+		shortDescription: 'Simsimi ChatBot by Simsimi.fun',
+		longDescription: {
+			en: 'Chat with simsimi'
+		},
+		category: 'sim',
+		guide: {
+			en: '   {pn} <word>: chat with simsimi'
+				+ '\n   Example:{pn} hi'
+		}
+	},
+
+	langs: {
+		en: {
+			chatting: 'Already Chatting with sim...',
+			error: 'Server Down Please Be Patient'
+		}
+	},
+
+	onStart: async function ({ args, message, event, getLang }) {
+		if (args[0]) {
+			const yourMessage = args.join(" ");
+			try {
+				const responseMessage = await getMessage(yourMessage);
+				return message.reply(`${responseMessage}`);
+			}
+			catch (err) {
+				console.log(err)
+				return message.reply(getLang("error"));
+			}
+		}
+	},
+
+	onChat: async ({ args, message, threadsData, event, isUserCallCommand, getLang }) => {
+		if (!isUserCallCommand) {
+			return;
+		}
+		if (args.length > 1) {
+			try {
+				const langCode = await threadsData.get(event.threadID, "settings.lang") || global.GoatBot.config.language;
+				const responseMessage = await getMessage(args.join(" "), langCode);
+				return message.reply(`${responseMessage}`);
+			}
+			catch (err) {
+				return message.reply(getLang("error"));
+			}
+		}
+	}
 };
-export const domain = {"sim": setup.name};
-let thread = {};
-export const execCommand = async function({api, event, key, kernel, args, keyGenerator, Users, context, translate, usage, prefix}) {
-  if(args.length === 0) return usage(this, prefix, event);
-  let True = (args[0] && args[0].toLowerCase() === "on") ? true: (args[0] && args[0].toLowerCase() === "off") ? false : null;
-  if(args.length === 0) return usage(this, prefix, event);
-  let msg = await translate((True === true) ? "enabled": "disabled", event, null, true);
-  if(True !== null) {
-    thread[event.threadID] = True;
-    return api.sendMessage(context+(await translate("✅ Sim successfully", event, null, true))+" "+msg, event.threadID, event.messageID);
-  }
-  let data = await kernel.read(["sim"], {key: key, id: event.senderID, ask: args.join(" ")});
-  return api.sendMessage((await translate(data.answer, event, null, false)), event.threadID, event.messageID)
+
+async function getMessage(yourMessage, langCode) {
+	try {
+		const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=ph&message=${yourMessage}&filter=false`);
+		if (!res.data.success) {
+			throw new Error('API returned a non-successful message');
+		}
+		return res.data.success;
+	} catch (err) {
+		console.error('Error while getting a message:', err);
+		throw err;
+	}
 }
-export const execEvent = async function({api, event, kernel, args, translate, key, cooldown, systemadmin}) {
-  if(!thread.hasOwnProperty(event.threadID)) thread[event.threadID] = false;
-  if((thread.hasOwnProperty(event.threadID) && thread[event.threadID] == false) || !systemadmin.includes(event.senderID) && cooldown.isCooldown(this.setup.name+event.senderID, this.setup.cooldown)) return; 
-  let data = await kernel.read(["sim"], {key: key, id: event.senderID, ask: args.join(" ")});
-  cooldown.create(this.setup.name+event.senderID);
-  return api.sendMessage((await translate(data.answer, event, null, false)), event.threadID, event.messageID)
-                  }
