b0847185829abf9892ace0e68c06f5791ada9768
NTKhang <nthanhkhang2003@gmail.com>
2024-05-06 18:21:57 +0700

    Update editMessage.js

--- fb-chat-api/src/editMessage.js
+++ fb-chat-api/src/editMessage.js
@@ -6,12 +6,12 @@
 const generateOfflineThreadingId = require('../utils');
 
 function canBeCalled(func) {
-  try {
-    Reflect.apply(func, null, []);
-    return true;
-  } catch (error) {
-    return false;
-  }
+	try {
+		Reflect.apply(func, null, []);
+		return true;
+	} catch (error) {
+		return false;
+	}
 }
 
 /**
@@ -21,46 +21,46 @@ function canBeCalled(func) {
  * @param {Object} callback - Callback for the function.
  */
 
-module.exports = function(defaultFuncs, api, ctx) {
-  return function editMessage(text, messageID, callback) {
-    if (!ctx.mqttClient) {
-      throw new Error('Not connected to MQTT');
-    }
+module.exports = function (defaultFuncs, api, ctx) {
+	return function editMessage(text, messageID, callback) {
+		if (!ctx.mqttClient) {
+			throw new Error('Not connected to MQTT');
+		}
 
-    ctx.wsReqNumber += 1;
-    ctx.wsTaskNumber += 1;
+		ctx.wsReqNumber += 1;
+		ctx.wsTaskNumber += 1;
 
-    const queryPayload = {
-      message_id: messageID,
-      text: text,
-    };
+		const queryPayload = {
+			message_id: messageID,
+			text: text
+		};
 
-    const query = {
-      failure_count: null,
-      label: '742',
-      payload: JSON.stringify(queryPayload),
-      queue_name: 'edit_message',
-      task_id: ctx.wsTaskNumber,
-    };
+		const query = {
+			failure_count: null,
+			label: '742',
+			payload: JSON.stringify(queryPayload),
+			queue_name: 'edit_message',
+			task_id: ctx.wsTaskNumber
+		};
 
-    const context = {
-      app_id: '2220391788200892',
-      payload: {
-        data_trace_id: null,
-        epoch_id: parseInt(generateOfflineThreadingId),
-        tasks: [query],
-        version_id: '6903494529735864',
-      },
-      request_id: ctx.wsReqNumber,
-      type: 3,
-    };
+		const context = {
+			app_id: '2220391788200892',
+			payload: {
+				data_trace_id: null,
+				epoch_id: parseInt(generateOfflineThreadingId),
+				tasks: [query],
+				version_id: '6903494529735864'
+			},
+			request_id: ctx.wsReqNumber,
+			type: 3
+		};
 
-    context.payload = JSON.stringify(context.payload);
+		context.payload = JSON.stringify(context.payload);
 
-    if (canBeCalled(callback)) {
-      ctx.reqCallbacks[ctx.wsReqNumber] = callback;
-    }
+		// if (canBeCalled(callback)) {
+		// 	ctx.reqCallbacks[ctx.wsReqNumber] = callback;
+		// }
 
-    ctx.mqttClient.publish('/ls_req', JSON.stringify(context), { qos: 1, retain: false });
-  }
-}
+		ctx.mqttClient.publish('/ls_req', JSON.stringify(context), { qos: 1, retain: false });
+	};
+};
