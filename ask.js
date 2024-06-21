06cdaacf00d537a0e8b381fd5666b630d7d9c354
Kongnathan <elpubliologuenathan@gmail.com>
2024-06-21 20:48:53 +0100

    Update ask.js

--- ask.js
+++ ask.js
@@ -36,7 +36,7 @@ async function getAIResponse(input, userId, messageID) {
 
 module.exports = {
   config: {
-    name: 'kyle',
+    name: 'ai',
     author: 'Arn',
     role: 0,
     category: 'ai',
