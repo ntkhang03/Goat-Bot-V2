91fab86cab0f5c18c39a1dad7bba827ece3b5de4
donnahuun <hedroxyydonnahuun@gmail.com>
2024-07-05 09:28:48 +0545

    Update war.js

--- scripts/cmds/war.js
+++ scripts/cmds/war.js
@@ -53,7 +53,31 @@ module.exports = {
         setTimeout(() => {
           a({ body: "TEORO AMAA LAAII KOPCHI MAA LAGERW CHIKAAMM RANNDDII MAKAA BAXHHA😭💋 " + name, mentions: arraytag });
         }, 500); // Reduced delay between messages to 500 milliseconds
-      }, 1000); // Reduced interval duration to 1000 milliseconds
+        setTimeout(() => {
+          a({ body: "TERO AAMA KO SIL TUTNEY GARII CHODNA NA PAROSSS MUJII KHATEYYY GAWAAARR 😂💦" + name, mentions: arraytag });
+        }, 1000); // Additional message after 1000 milliseconds
+        setTimeout(() => {
+          a({ body: "MUJI GAREEEB KO BACCHA SALLA XAKKAAA MUJIII AUKAT LESSSS EKSIKKO AUKAT NA BHAKO BUNU 😂💋 " + name, mentions: arraytag });
+        }, 1500); // Additional message after 1500 milliseconds
+        setTimeout(() => {
+          a({ body: "TA MUJII KTA SAMAJ MAH KASARI AAYIS TEI SOCHI RAXUHH MUJII PUTI BHAKO MANXEYY YAAA PUTI FLEX HANNA NA AAIJO 😂💋 " + name, mentions: arraytag });
+        }, 2000); // Additional message after 2000 milliseconds
+        setTimeout(() => {
+          a({ body: "SALA GAWARR TERO JASTO TW MERO BHUTRA SAMAN HOOO JHAT HARU MUJI 2 3 CLASS MAH DNEY FUCCHA FUCHHI BAU SANGA NEU KHOJXA 😂😂💋 " + name, mentions: arraytag });
+        }, 2500); // Additional message after 2500 milliseconds
+        setTimeout(() => {
+          a({ body: "GAWARR KHATEYYY KOO XOROO MUJII TAILET LADOO CHUSNA BAHEK K GAREKO XASS HMM MUJII ? 😂😂💋 KHATEYY SALLA JAAA GAYERW BLOUSE LAGAYERW GHUMMM 😂👌 " + name, mentions: arraytag });
+        }, 3000); // Additional message after 3000 milliseconds
+        setTimeout(() => {
+          a({ body: "MUJI THAMEL KO MAAAL SALA GAWARRR KO NASOOOOO AUKAT LESSS TERO CHAK MAH MERO LADO KO JHATYARO HANNU PARNEY BELA BHAYO 😂👌 " + name, mentions: arraytag });
+        }, 3500); // Additional message after 3500 milliseconds
+        setTimeout(() => {
+          a({ body: "HEER BABU TW MUJI KO AUKAT XAINA BUJHIS AHILEY TESAILEY BHANDAI XUH MUJI AAFNO PUTI FLX NA HAAAN CHIKNA TIME LAGAUDANA 😂👌" + name, mentions: arraytag });
+        }, 4000); // Additional message after 4000 milliseconds
+        setTimeout(() => {
+          a({ body: "SALA RATNA PARK KOO MAAAL MUJI TERO RATE KATI HO BHANTWWW ? MUJII PUTI FLEX HANDAI HIDXAS RANDI KAAA BAAAN 😂😂👌 " + name, mentions: arraytag });
+        }, 4500); // Additional message after 4500 milliseconds
+      }, 5000); // Adjusted interval duration to 5000 milliseconds to accommodate all messages
     } else if (!userData.warMode) {
       return api.sendMessage("War Mode is currently deactivated.", event.threadID);
     } else {
